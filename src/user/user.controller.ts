0a23be422e545a3a663355a4f4117fc560f35dfc
malakhovarthur <malakhov051@gmail.com>
2024-06-16 09:10:22 +0300

    add same sit set

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -191,6 +191,7 @@ export class UserController {
       res.cookie("refreshToken", userData.refreshToken, {
         maxAge: 30 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        sameSite: "strict",
       });
       res.json(userData);
     } catch (err) {
